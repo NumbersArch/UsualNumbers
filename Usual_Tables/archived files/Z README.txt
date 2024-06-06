b1dfb3904f4076e3fbbf169b96017827bf9fcf5d
NumbersArch <qjq3WkpZP9mP6hhbLV0r4mirbXo4XL@gmail.com>
2024-06-05 17:16:03 -0700

    Add files via upload

---
+++ Usual_Tables/archived files/Z README.txt
@@ -0,0 +1,2 @@
+Storing some reference files in case the ones in main folder get deleted somehow.
+scraper_loop.sh takes two dates and runs "scraper" between them. Place in main folder to run. 
---
+++ Usual_Tables/timechart.sh
@@ -0,0 +1,54 @@
+#!/bin/bash  
+#bash timechart.sh "10/2023" "05/2024"
+
+proceed=true
+if [ "$#" -lt 2 ]; then
+    echo "Needs 2 date parameters (month/year)"
+    proceed=false  
+fi
+if [ "$#" -gt 2 ]; then
+    echo "Too many parameters"
+    proceed=false  
+fi
+
+if [ "$proceed" = true ]; then
+
+date1="$1"
+date1assigned=false
+if [[ $date1 == *"-"* ]]; then
+	month1="$(echo "$date1" | cut -d'-' -f1)"; month1="$( echo "${month1#0}")"
+	year1="$(echo "$date1" | cut -d'-' -f2)"
+	
+	date1assigned=true
+fi
+if [[ $date1 == *"/"* ]]; then
+	month1="$(echo "$date1" | cut -d/ -f1)"; month1="$( echo "${month1#0}")"
+	year1="$(echo "$date1" | cut -d/ -f2)"
+	date1assigned=true
+fi
+
+date2="$2"
+date2assigned=false
+if [[ $date2 == *"-"* ]]; then
+	month2="$(echo "$date2" | cut -d'-' -f1)"; month2="$( echo "${month2#0}")"
+	year2="$(echo "$date2" | cut -d'-' -f2)"
+	
+	date2assigned=true
+fi
+if [[ $date2 == *"/"* ]]; then
+	month2="$(echo "$date2" | cut -d/ -f1)"; month2="$( echo "${month2#0}")"
+	year2="$(echo "$date2" | cut -d/ -f2)"
+	date2assigned=true
+fi
+
+if [ "$date1assigned" = false ] || [ "$date2assigned" = false ]  ; then
+echo "dates input improperly"
+else
+
+datestring1="$year1-$month1"
+datestring2="$year2-$month2"
+
+python3 .py/assemble_timechart.py "$datestring1" "$datestring2"
+
+fi
+fi
